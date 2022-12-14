ef9d2d5e55dfec8e071af990ff07707121cef502
Blinina <anastasia.paancake@gmail.com>
2022-12-14 15:29:10 +0300

    main form and genDiff

---
+++ my-app/src/App.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useForm, SubmitHandler } from "react-hook-form";
+import getDiff from './getDiff.js';
+
+type Inputs = {
+  file1: string,
+  file2: string,
+};
+
+
+function App() {
+  const [result, setResult] = useState('...');
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    getDiff(data);
+  }
+
+  return (
+    <div >
+      <h2>File difference</h2>
+      <div className="app">
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <div className='form-colomn'>
+            <Form.Group className="mb-3 textarea" controlId="file1">
+              <Button variant="outline-primary">Primary</Button>
+
+              <Form.Label>file1</Form.Label>
+              <Form.Control as="textarea"
+                {...register("file1", { required: true })} />
+            </Form.Group>
+
+            <Form.Group className="mb-3 textarea" controlId="file2">
+              <Button variant="outline-primary">Primary</Button>
+              <Form.Label>file2</Form.Label>
+              <Form.Control as="textarea"
+                {...register("file2", { required: true })} />
+            </Form.Group>
+          </div>
+          <Button variant="primary" type="submit" onClick={handleSubmit(onSubmit)}>
+            Difference
+          </Button>
+        </Form>
+      </div>
+      {result}
+    </div>
+  );
+}
+
+export default App;
